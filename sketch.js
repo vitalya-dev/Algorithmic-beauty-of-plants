5fde3035872523118706f5d918dbb5fb689f794e
Vitalya Dev <vitalya.dev@gmail.com>
2025-10-16 08:55:28 +0300

    Implement the 3D Turtle

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
-// NEW: Constants for the Honda tree model from the book
-const n = 10; // This will control the number of generations, but we'll still use the button for now.
+// Constants for the Honda tree model from the book
 const r1 = 0.9; // Contraction ratio for the trunk
 const r2 = 0.6; // Contraction ratio for branches
 const a0 = 45;  // Branching angle from the trunk
@@ -7,59 +6,37 @@ const a2 = 45;  // Branching angle for lateral axes
 const s = 137.5; // Divergence angle
 const wr = 0.707; // Width decrease rate
 
-// NEW: The axiom is now an array of objects
+// The axiom is an array of objects
 let axiom = [{ char: 'A', params: [100, 10] }]; // Start with length 100, width 10
 let sentence = axiom;
-
 let generation = 0;
 
-// NEW: Rules are now functions that return arrays of new module objects.
+// Rules are functions that return arrays of new module objects.
 const rules = {
-	// A(l,w) -> !(w)F(l)[&(a0)B(l*r2,w*wr)]/(s)A(l*r1,w*wr)
 	A: (l, w) => [
-		{ char: '!', params: [w] },
-		{ char: 'F', params: [l] },
-		{ char: '[' },
-		{ char: '&', params: [a0] },
-		{ char: 'B', params: [l * r2, w * wr] },
-		{ char: ']' },
-		{ char: '/', params: [s] },
-		{ char: 'A', params: [l * r1, w * wr] }
+		{ char: '!', params: [w] }, { char: 'F', params: [l] }, { char: '[' },
+		{ char: '&', params: [a0] }, { char: 'B', params: [l * r2, w * wr] }, { char: ']' },
+		{ char: '/', params: [s] }, { char: 'A', params: [l * r1, w * wr] }
 	],
-	// B(l,w) -> !(w)F(l)[-(a2)$C(l*r2,w*wr)]C(l*r1,w*wr)
 	B: (l, w) => [
-		{ char: '!', params: [w] },
-		{ char: 'F', params: [l] },
-		{ char: '[' },
-		{ char: '-', params: [a2] },
-		{ char: '$' },
-		{ char: 'C', params: [l * r2, w * wr] },
-		{ char: ']' },
+		{ char: '!', params: [w] }, { char: 'F', params: [l] }, { char: '[' },
+		{ char: '-', params: [a2] }, { char: '$' }, { char: 'C', params: [l * r2, w * wr] }, { char: ']' },
 		{ char: 'C', params: [l * r1, w * wr] }
 	],
-	// C(l,w) -> !(w)F(l)[+(a2)$B(l*r2,w*wr)]B(l*r1,w*wr)
 	C: (l, w) => [
-		{ char: '!', params: [w] },
-		{ char: 'F', params: [l] },
-		{ char: '[' },
-		{ char: '+', params: [a2] },
-		{ char: '$' },
-		{ char: 'B', params: [l * r2, w * wr] },
-		{ char: ']' },
+		{ char: '!', params: [w] }, { char: 'F', params: [l] }, { char: '[' },
+		{ char: '+', params: [a2] }, { char: '$' }, { char: 'B', params: [l * r2, w * wr] }, { char: ']' },
 		{ char: 'B', params: [l * r1, w * wr] }
 	]
 };
 
 function setup() {
 	createCanvas(600, 600, WEBGL);
-	
 	const button = createButton('Generate Next');
 	button.mousePressed(generate);
-	
-	drawFractal(); // Initial draw
+	drawFractal();
 }
 
-// NEW: The generate function is rewritten to handle the array of objects
 function generate() {
 	generation++;
 	let nextSentence = [];
@@ -67,38 +44,72 @@ function generate() {
 	for (const module of sentence) {
 		const rule = rules[module.char];
 		if (rule) {
-			// If a rule exists, call it with the module's parameters
 			const newModules = rule(...module.params);
 			nextSentence.push(...newModules);
 		} else {
-			// If no rule, it's a constant like '[', ']', '+', etc. Just copy it.
 			nextSentence.push(module);
 		}
 	}
 	sentence = nextSentence;
-	// You can uncomment the line below to see the generated sentence in the console!
-	// console.log(sentence); 
+
+	// ADDED: Debug message to see the sentence in the console
+	console.log(`Generation ${generation}:`, sentence); 
+	
 	drawFractal(); 
 }
 
-// This function tells the turtle how to draw based on the sentence
-// NOTE: This function is now BROKEN and will be fixed in Subtask 3.
+// NEW: The turtle function is now fully implemented for 3D!
 function turtle() {
-	// The old code looping through a string will not work here.
-	// We leave it empty for now.
+	for (const module of sentence) {
+		switch (module.char) {
+			case '!': // Set line width
+				strokeWeight(module.params[0]);
+				break;
+			case 'F': // Move forward and draw a line
+				// Y is the 'up' direction in p5.js 3D, so we move along negative Y
+				line(0, 0, 0, 0, -module.params[0], 0);
+				translate(0, -module.params[0], 0);
+				break;
+			case '+': // Turn Right (Yaw)
+				rotateY(radians(module.params[0]));
+				break;
+			case '-': // Turn Left (Yaw)
+				rotateY(radians(-module.params[0]));
+				break;
+			case '&': // Pitch Down
+				rotateX(radians(module.params[0]));
+				break;
+			case '^': // Pitch Up
+				rotateX(radians(-module.params[0]));
+				break;
+			case '/': // Roll Right
+				rotateZ(radians(module.params[0]));
+				break;
+			case '\\': // Roll Left
+				rotateZ(radians(-module.params[0]));
+				break;
+			case '$': // Roll 180 degrees
+				rotateZ(radians(180));
+				break;
+			case '[': // Push state
+				push();
+				break;
+			case ']': // Pop state
+				pop();
+				break;
+		}
+	}
 }
 
-// Central function to handle all the drawing logic
+
 function drawFractal() {
 	background(50);
 	resetMatrix();
-	// Let's position the origin a bit lower on the screen
 	translate(0, 200, 0);
-	// We also rotate it slightly on the X-axis to get a better initial view
 	rotateX(radians(-30));
 	
 	stroke(255);
-	turtle(); // Draw the fractal
+	turtle();
 }
 
 function draw() {
