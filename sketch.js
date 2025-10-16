aaa57517da37987710dede62ebf0815b0b3478f8
Vitalya Dev <vitalya.dev@gmail.com>
2025-10-16 08:39:15 +0300

    Convert the L-System to be Parametric

--- sketch.js
+++ sketch.js
@@ -1,23 +1,57 @@
-// L-System variables
-let axiom = 'X';
-let sentence = axiom;
-let len = 150;
-let angle;
-let generation = 0;
-// We will manage this variable inside the turtle function
-let strokeWidth; 
+// NEW: Constants for the Honda tree model from the book
+const n = 10; // This will control the number of generations, but we'll still use the button for now.
+const r1 = 0.9; // Contraction ratio for the trunk
+const r2 = 0.6; // Contraction ratio for branches
+const a0 = 45;  // Branching angle from the trunk
+const a2 = 45;  // Branching angle for lateral axes
+const s = 137.5; // Divergence angle
+const wr = 0.707; // Width decrease rate
 
+// NEW: The axiom is now an array of objects
+let axiom = [{ char: 'A', params: [100, 10] }]; // Start with length 100, width 10
+let sentence = axiom;
+
+let generation = 0;
+
+// NEW: Rules are now functions that return arrays of new module objects.
 const rules = {
-	X: 'F+[[X]-X]-F[-FX]+X',
-	F: 'FF'
+	// A(l,w) -> !(w)F(l)[&(a0)B(l*r2,w*wr)]/(s)A(l*r1,w*wr)
+	A: (l, w) => [
+		{ char: '!', params: [w] },
+		{ char: 'F', params: [l] },
+		{ char: '[' },
+		{ char: '&', params: [a0] },
+		{ char: 'B', params: [l * r2, w * wr] },
+		{ char: ']' },
+		{ char: '/', params: [s] },
+		{ char: 'A', params: [l * r1, w * wr] }
+	],
+	// B(l,w) -> !(w)F(l)[-(a2)$C(l*r2,w*wr)]C(l*r1,w*wr)
+	B: (l, w) => [
+		{ char: '!', params: [w] },
+		{ char: 'F', params: [l] },
+		{ char: '[' },
+		{ char: '-', params: [a2] },
+		{ char: '$' },
+		{ char: 'C', params: [l * r2, w * wr] },
+		{ char: ']' },
+		{ char: 'C', params: [l * r1, w * wr] }
+	],
+	// C(l,w) -> !(w)F(l)[+(a2)$B(l*r2,w*wr)]B(l*r1,w*wr)
+	C: (l, w) => [
+		{ char: '!', params: [w] },
+		{ char: 'F', params: [l] },
+		{ char: '[' },
+		{ char: '+', params: [a2] },
+		{ char: '$' },
+		{ char: 'B', params: [l * r2, w * wr] },
+		{ char: ']' },
+		{ char: 'B', params: [l * r1, w * wr] }
+	]
 };
 
-// The setup function runs once when the sketch starts
 function setup() {
-	// MODIFIED: Added WEBGL to enable 3D mode
-	createCanvas(600, 600, WEBGL); 
-	
-	angle = radians(25);
+	createCanvas(600, 600, WEBGL);
 	
 	const button = createButton('Generate Next');
 	button.mousePressed(generate);
@@ -25,72 +59,48 @@ function setup() {
 	drawFractal(); // Initial draw
 }
 
-// This function generates the next iteration of the sentence
+// NEW: The generate function is rewritten to handle the array of objects
 function generate() {
-	len *= 0.55;
 	generation++;
+	let nextSentence = [];
 	
-	let nextSentence = '';
-	for (let i = 0; i < sentence.length; i++) {
-		const current = sentence.charAt(i);
-		if (rules.hasOwnProperty(current)) {
-			nextSentence += rules[current];
+	for (const module of sentence) {
+		const rule = rules[module.char];
+		if (rule) {
+			// If a rule exists, call it with the module's parameters
+			const newModules = rule(...module.params);
+			nextSentence.push(...newModules);
 		} else {
-			nextSentence += current;
+			// If no rule, it's a constant like '[', ']', '+', etc. Just copy it.
+			nextSentence.push(module);
 		}
 	}
 	sentence = nextSentence;
-	drawFractal(); // Redraw with the new sentence
+	// You can uncomment the line below to see the generated sentence in the console!
+	// console.log(sentence); 
+	drawFractal(); 
 }
 
 // This function tells the turtle how to draw based on the sentence
+// NOTE: This function is now BROKEN and will be fixed in Subtask 3.
 function turtle() {
-	for (let i = 0; i < sentence.length; i++) {
-		const current = sentence.charAt(i);
-
-		if (current == 'F') {
-			strokeWeight(strokeWidth);
-			line(0, 0, 0, 0, -len, 0); // Note: line() in 3D takes 6 arguments
-			translate(0, -len, 0);
-		} else if (current == '+') {
-			rotateZ(angle);
-		} else if (current == '-') {
-			rotateZ(-angle);
-		} else if (current == '[') {
-			push(); // Save position and angle
-			strokeWidth *= 0.7; 
-		} else if (current == ']') {
-			strokeWidth /= 0.7; 
-			pop();  // Restore position and angle
-		}
-	}
+	// The old code looping through a string will not work here.
+	// We leave it empty for now.
 }
 
 // Central function to handle all the drawing logic
 function drawFractal() {
 	background(50);
 	resetMatrix();
-	translate(0, 300 , 0); 
-
-	
-	// The text display part is a bit trickier in WEBGL, so we'll comment it out for now.
-	// fill(255);
-	// noStroke();
-	// text('Generation: ' - + generation, 10, 20);
+	// Let's position the origin a bit lower on the screen
+	translate(0, 200, 0);
+	// We also rotate it slightly on the X-axis to get a better initial view
+	rotateX(radians(-30));
 	
 	stroke(255);
-	
-	strokeWidth = 4;
-
-	// In 3D, we don't need to resetMatrix() or translate to the bottom center
-	// The orbitControl centers the view for us.
-	// resetMatrix();
-	// translate(width / 2, height); 
-	
 	turtle(); // Draw the fractal
 }
 
-// The draw function is not used in this version
 function draw() {
 	noLoop();
 }
