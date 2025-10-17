20e301a2c2a36e43d1c2c767a41094324537ea34
Vitalya Dev <vitalya.dev@gmail.com>
2025-10-17 08:54:13 +0300

    Testing Pitch.

--- sketch.js
+++ sketch.js
@@ -1,25 +1,24 @@
-// A variable to hold the sequence of drawing commands.
 let sentence;
 
 function setup() {
 	createCanvas(600, 600, WEBGL);
-	//debugMode()
-	// This is our test sentence to draw a square using Yaw turns.
+	
+	// Test sentence for Pitch: F&F&F&F
 	// F: Draw forward
-	// +: Turn right (Yaw) 90 degrees
+	// &: Pitch down 90 degrees
 	sentence = [
-		{ char: '!', params: [3] },      // Set line width to 3
+		{ char: '!', params: [3] },      // Set line width
 		{ char: 'F', params: [150] },    // Forward 150
-		{ char: '+', params: [90] },     // Turn right 90 degrees
+		{ char: '&', params: [90] },     // Pitch down 90 degrees
 		{ char: 'F', params: [150] },    // Forward 150
-		{ char: '+', params: [90] },     // Turn right 90 degrees
+		{ char: '&', params: [90] },     // Pitch down 90 degrees
 		{ char: 'F', params: [150] },    // Forward 150
-		{ char: '-', params: [90] },     // Turn right 90 degrees
+		{ char: '&', params: [90] },     // Pitch down 90 degrees
 		{ char: 'F', params: [150] }     // Forward 150
 	];
 }
 
-// Interprets the 'sentence' array and draws the corresponding 3D graphics.
+// Your corrected turtle function!
 function turtle() {
 	for (const module of sentence) {
 		switch (module.char) {
@@ -30,52 +29,46 @@ function turtle() {
 				line(0, 0, 0, 0, 0, -module.params[0]);
 				translate(0, 0, -module.params[0]);
 				break;
-			case '+': // Turn Right (Yaw) -> Rotates around turtle's Up (Y-axis)
+			case '+': // Turn Right (Yaw)
 				rotateY(radians(-module.params[0]));
 				break;
-			case '-': // Turn Left (Yaw) -> Rotates around turtle's Up (Y-axis)
+			case '-': // Turn Left (Yaw)
 				rotateY(radians(module.params[0]));
 				break;
-			case '&': // Pitch Down -> Rotates around turtle's Left (X-axis)
+			case '&': // Pitch Down
 				rotateX(radians(module.params[0]));
 				break;
-			case '^': // Pitch Up -> Rotates around turtle's Left (X-axis)
+			case '^': // Pitch Up
 				rotateX(radians(-module.params[0]));
 				break;
-			case '/': // Roll Right -> Rotates around turtle's Heading (Z-axis)
+			case '/': // Roll Right
 				rotateZ(radians(module.params[0]));
 				break;
-			case '\\': // Roll Left -> Rotates around turtle's Heading (Z-axis)
+			case '\\': // Roll Left
 				rotateZ(radians(-module.params[0]));
 				break;
 			case '$': // Roll 180 degrees
 				rotateZ(radians(180));
 				break;
-			case '[': // Push the current transformation matrix onto the stack
+			case '[': // Push state
 				push();
 				break;
-			case ']': // Pop the current transformation matrix from the stack
+			case ']': // Pop state
 				pop();
 				break;
 		}
 	}
 }
 
-// Sets up the 3D scene and calls the turtle to draw.
 function drawFractal() {
 	background(50);
-	
-	// Use orbitControl for interactive camera
 	orbitControl();
 	
-	// We reset the matrix and draw the turtle at the center of the world
 	resetMatrix();
 	stroke(255);
 	turtle();
 }
 
-// The draw loop now continuously calls drawFractal to update the scene
-// and listen for orbitControl inputs.
 function draw() {
 	drawFractal();
 }
